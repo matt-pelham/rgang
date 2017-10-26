1111c8cd036ce986a59eb6596a0568d11f09c32e
cburger90 <cburger90@yahoo.com>
2017-10-26 08:39:00 -0500

    Add files via upload

--- project.R
+++ project.R
@@ -16,20 +16,17 @@ print(p1)
 p2 <- densityplot.defaultrate.over.tuition(unflattened_df)
 print(p2)
 
-p3 <- plot.combined.default.over.tuition(unflattened_df)
-print(p3)
-ggsave(filename = "defaultovertuition.png", plot = p3, width = 6, height = 4, dpi = 600)
 
-p4 <- plot.default.rate.over.degree.and.tuition(unflattened_df)
+p4 <- plot.default.rate.over.degree.and.tuition(unflattened_df) 
 print(p4)
 ggsave(filename = "defaultrate-degree-tuition.png", plot = p4, width = 6, height = 4, dpi = 600)
 
-pboxplot <- boxplot.default.rates.v.program.length(unflattened_df) 
-ggsave(filename = "ProgLen.png", plot = pboxplot, width = 7, height = 4, dpi = 600)
+
 
 generate.summary.tables(unflattened_df)
 
 p5 <- plot.default.over.tuition(unflattened_df)
+print(p5)
 ggsave(filename = "Ben1.png", plot = p5, width = 6, height = 4, dpi = 600)
 
 p6 <- plot.default.over.tuition.typefacet(unflattened_df)
