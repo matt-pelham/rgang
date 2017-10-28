ca438c28de676a2e679742ac174589d4646dae33
bhuntington <31260785+bhuntington@users.noreply.github.com>
2017-10-28 00:35:36 -0500

    Update schooltypeanalysis.R

--- schooltypeanalysis.R
+++ schooltypeanalysis.R
@@ -8,73 +8,54 @@ generate.summary.tables <- function(df){
   levels(df$School.Type)[levels(df$School.Type) == "2"] <- "Private"
   levels(df$School.Type)[levels(df$School.Type) == "3"] <- "Proprietary"
   
-  #With just three levels that remained after we sifted for the complete cases, I decided just #rename the levels...
+  # After joining together our datasets and sifting through out incomplete obervations, we were left with just three levels of our
+  # School.Type variable among our complete cases. To produce analysis that was easier to comprehend, I decided to just rename levels
+  # to their categorical values contained in our datakey. The nubering system used by the department of education seemed to be of little
+  # value for our purposes. 
+
   
   df <-group_by(df, School.Type, Year)
   df_s <- summarize(df, avg_default = mean(CohortDefaultRate))
   df_st <-dcast(df_s, School.Type ~ Year, value.var = "avg_default")
   assign("summary.defaultrates.by.schooltype",df_st,envir = .GlobalEnv)
   df <- ungroup(df)
+ 
   # Public Colleges and Universities had the highest average default rates for years
   # 2012 and 2013. Private-Non Profit Colleges and Universities had the lowest deafult rates-
   # almost half that of it Public and Proprietary counterparts. This summary table alone
   # shows that private institutions outpace their counterparts as it relates to default rate.
+
   
-  df1 <-subset(df, School.Type == "Public")
-  df1 <-group_by(df1, School.Type, Year)
+  df1 <-group_by(df, School.Type, Year)
   df_s1 <- summarize(df1, perc_default = sum(CohortDefaultRate <= 15)/n())
   df_st1 <- dcast(df_s1, School.Type ~ Year, value.var = "perc_default")
-  assign("summary.public.percent.default15",df_st1,envir = .GlobalEnv)
+ 
+  assign("summary.percent.default15",df_st1,envir = .GlobalEnv)
+
   
-  df2 <-subset(df, School.Type == "Private")
-  df2 <-group_by(df2, School.Type, Year)
-  df_s2 <- summarize(df2, perc_default = sum(CohortDefaultRate <= 15)/n())
+  # The summary table above captured how each school type is meeting the Department of Education
+  # benchmark of a sub-15% default rate.Schools who are meeting that benchmark are able to ride themselves
+  # of the restrictions surrounding the dispersment of student loans. These tables capture the number of 
+  # institutions (within each school type) that carry sub-15% default rates over the total number of insitutions
+  # in that same school type (public, private, proprietary). Private schools appear most likely to achieve this benchmark. 
+  # Public schools rates for this metric decreased from 2012 to 2013. The percentage of Proprietary schools meeting this benchmark 
+  # increased from year 2012 to 2013. 
+
+  
+  df2 <-group_by(df, School.Type, Year)
+  df_s2 <- summarize(df2, perc_default = sum(CohortDefaultRate >=30)/n())
   df_st2 <- dcast(df_s2, School.Type ~ Year, value.var = "perc_default")
-  assign("summary.private.percent.default15",df_st2,envir = .GlobalEnv)
   
-  df3 <-subset(df, School.Type == "Proprietary")
-  df3 <-group_by(df3, School.Type, Year)
-  df_s3 <- summarize(df3, perc_default = sum(CohortDefaultRate <= 15)/n())
-  df_st3 <- dcast(df_s3, School.Type ~ Year, value.var = "perc_default")
-  assign("summary.proprietary.percent.default15",df_st3,envir = .GlobalEnv)
-  
-  #Based on the Summary Tables above, we can see that Private-Non Profit Colleges
-  #and Universities are most likely to rid themselves of restrictions as it relates to 
-  #dispersing financial aid in the form of loans. If an institution maintains
-  # a cohort default rate below 15% for at least three consecutive years, they acquire the
-  #autonomy to disperse loan monies. If an institution does not achieve this benchmark
-  # they must wait to 30 days to disburse loan monies to first year or first-time borrowers.
-  # Within this metric, Public Schools, improved from year to year, showing an increae in the #percentage of 
-  # schools with default rates <=15%. Private Schools faltered slightly. Proprietary schools #remained the same. 
-  
-  
-  
-  df4 <-subset(df, School.Type == "Public")
-  df4 <-group_by(df4, School.Type, Year)
-  df_s4 <- summarize(df4, perc_default = sum(CohortDefaultRate >=30)/n())
-  df_st4 <- dcast(df_s4, School.Type ~ Year, value.var = "perc_default")
-  
-  assign("summary.public.percent.default30",df_st4,envir = .GlobalEnv)
-  
-  df5 <-subset(df, School.Type == "Private")
-  df5 <-group_by(df5, School.Type, Year)
-  df_s5 <- summarize(df5, perc_default = sum(CohortDefaultRate >=30)/n())
-  df_st5 <- dcast(df_s5, School.Type ~ Year, value.var = "perc_default")
-  
-  assign("summary.private.percent.default30",df_st5,envir = .GlobalEnv)
-  
-  df6 <-subset(df, School.Type == "Proprietary")
-  df6 <-group_by(df6, School.Type, Year)
-  df_s6 <- summarize(df6, perc_default = sum(CohortDefaultRate >=30)/n())
-  df_st6 <- dcast(df_s6, School.Type ~ Year, value.var = "perc_default")
-  assign("summary.proprietary.percent.default30",df_st6,envir = .GlobalEnv)
-  
-  # Based on the Summary Tables above, we can see that in 2012, Public Colleges and most 
-  # Universities were most likely to lose eligibility to participate in federal student 
-  # loan and/or Federal Pell Grant programs.In 2013, Proprietary Colleges and Universities
-  # were most likely to lose eligibilty to participate in the same programs. Schools are at risk of #suffering this sanction 
-  # if their default rate is at or above 30%. Year to year, PUblic instutions improved on this metric. 
-  # Private instutions faltered slightly, as did Proprietary Schools. 
+  assign("summary.percent.default30",df_st2,envir = .GlobalEnv)
+
+ 
+ 
+  # The summary table above captures the percentage of each school type that is 'at-risk' of losing eligibility 
+  # to participate in federal student loan and/or Federal Pell Grant programs. Schools that have three consecutive years
+  # of cohort loan default rates above 30% could lose the ability to particiapte in these programs. All groups appeared to perform well
+  # in this metric as very small percentage of these instituions have default rates above 30%.Within our dataset, Public colleges were
+  # most likely to lose eligbility in the programs, though that group improved from 2012 to 2013. Private colleges also improved from 
+  # year to year. Proprietary collegs performed best in this metric, but there were fewer observaions among this level. 
 }
 
 
@@ -87,14 +68,12 @@ plot.default.over.tuition <- function(df)
   p <- p + ggtitle("Student Loan Default Rate vs. Tuition (2012 - 2013)")
   p <- p + theme(plot.title = element_text(size = 12, face = "bold"))
   p <- p + theme(axis.title = element_text(size = 12, face = "bold"))
-  p
+  p <- p + scale_color_manual(values=c("light blue", "orange", "black"))
 }
 
+# This scatter plot looks at 3 variables-default rate, tuition and school type. Here, we can see that Public institutions, with a lower
+# cost of attendance show higher default rates. 
 
-
-
-# This plot looks at 3 variables-default rate, tuition and school type. I thought
-# it was a good visual representation of the questions both Cody and I were assigned to analyze
 plot.default.over.tuition.typefacet <- function(df){
   bp <- qplot(School.Type, CohortDefaultRate, data = df, geom = "boxplot") + geom_jitter(alpha = I(.10))
   bp <- bp + xlab("School Type")
@@ -104,10 +83,8 @@ plot.default.over.tuition.typefacet <- function(df){
   bp <- bp + theme(axis.title = element_text(size = 12, face = "bold"))
   bp 
  
-  # This plot shows a little more about what's going on with the school type distribution. 
-  
-  # With just three levels in my school.type column, I don't know that my analysis is all that #interesting, 
-  # which is why I tried to contextualize as much as I could with the benchmark rates (<15% and #>30%). 
+  # This box plot shows a little more about what's going with default rates with each individual school type. Here, we have a good
+  # picture regarding mean default rates for each school type, the density of each quartile and also outlier values for each school type.
 }
 
 
