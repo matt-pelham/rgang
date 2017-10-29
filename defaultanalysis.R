6f919662b00f595b4c5f1f52fe4607c22109ff05
cburger90 <cburger90@yahoo.com>
2017-10-29 10:01:45 -0600

    Add files via upload

--- defaultanalysis.R
+++ defaultanalysis.R
@@ -11,7 +11,7 @@ densityplot.default.over.tuition <- function(df){
   #P1   Density plot to show that lower tuition rates have a higher default rate
   p <- qplot(Tuition,NumInDefault , data = df, geom = "bin2d")
   p <- p + scale_fill_gradient(name = "Frequency", low = "blue", high = "red")
-  p <- p + ggtitle("Density Map Measuring Default Rates Against Cost of Tuition ")
+  p <- p + ggtitle("Number of Students In Default vs. Cost of Tuition (2012 - 2103) ") 
   p <- p + xlab("Cost of Tuition ")
   p<- p + ylab("Number in Default")
   p
@@ -43,8 +43,8 @@ densityplot.defaultrate.over.tuition <- function(df){
   
   #P2   Produces a log scale which shows the default percentage compared against tuition binned.   Shows that as the tuiion goes up,
   # the defualt rates are less dense at the higher percentages.
-  p <- qplot(CohortDefaultRate,  data = df, geom = "density", facets = . ~ Tuition_binned )
-  p <- p + ggtitle("Density Plot of Default Rates for Binned Tuition Prices")
+  p <- qplot(CohortDefaultRate,  data = df, geom = "density", facets = . ~ Tuition_binned)
+  p <- p + ggtitle ("Student Loan Default Rate vs. Cost of Tuition Binned (2012 - 2013)")  
   p <- p + xlab("Default Rates")
   p <- p + theme(plot.title = element_text(face = "bold"))
   p <- p + theme(panel.grid = element_blank())
@@ -66,13 +66,13 @@ plot.default.rate.over.degree.and.tuition <- function(df){
   #Shows that associates degrees have a higher default rate despite their lower tuition.   In conclusion
   #lower default rates are associated with higher tuition rates along with Bachelor degrees and Master's Degrees.
   #Higher default rates are associated with lower tuition and associates degrees.
-
+  
   p <- qplot(Tuition, CohortDefaultRate, data = unflattened_df, geom = "point", color= Prog.Meaning,
              alpha= I(.50))
-  p <- p + ggtitle("Default Rate over Cost of Tuition")
-  p <- p + scale_x_continuous(name = "Cost of Tuition",
+  p <- p + ggtitle("Student Loan Default Rate v. Cost of Tuition (2012 - 2013")
+  p <- p + scale_x_continuous(name = "Cost of Tuition (in U.S. Dollars)",
                               breaks= 5000*(0:9), labels = dollar)
-  p <- p + scale_y_continuous(name = "Default Rate (%)")
+  p <- p + scale_y_continuous(name = "Default Rate (Percentage)")
   p <- p + theme(panel.border = element_rect(linetype = "solid",
                                              color = "black", fill = NA))
   p <- p + theme(legend.text = element_text(size = 8))
@@ -82,8 +82,19 @@ plot.default.rate.over.degree.and.tuition <- function(df){
   p <- p + theme(legend.key = element_rect(fill = "white"))
   p <- p + theme(plot.title = element_text(face = "bold"))
   p <- p + theme(panel.grid = element_blank())
-  p <- p + scale_color_hue (name = "Program Length")
+  #p <- p + scale_color_hue (name = "Program Length")
   p <- p + theme(axis.text.x = element_text(angle = -90))
+  p <- p + scale_color_manual(name = "Program Length", values=c(
+                                        "blue",
+                                        "maroon",
+                                        "light blue",
+                                        "orange",
+                                        "purple",
+                                        "black",
+                                        "dark gray",
+                                        "purple",
+                                        "green"))
+
   p
-}  
+} 
 
